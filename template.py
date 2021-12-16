617db95157e5ffa6b2a044ea9323719e78f7924f
koenlam <47334582+koenlam@users.noreply.github.com>
2021-12-16 20:56:01 +0100

    Added some useful libaries to the template

--- template.py
+++ template.py
@@ -1,16 +1,20 @@
 #! /usr/bin/env python3
+import re
 import numpy as np
+
+from collections import defaultdict
+from functools import reduce
+
 
 def read_file(filename):
     with open(filename) as f:
         return f.read().strip().split("\n")
 
 
-
 def part1(input_file):
     print("Part 1")
     x = read_file(input_file)
-
+    print(x)
 
 
 def part2(input_file):
